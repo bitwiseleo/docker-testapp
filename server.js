2bd1a05e47b4348c192a16cc9167ba6d1cef3734
“bitwiseleo” <bitwiseleo@gmail.com>
2025-12-04 18:05:18 -0500

    Added docker-compose, Dockerfile and updated them to use environment variables from .env

--- server.js
+++ server.js
@@ -3,40 +3,44 @@ const app = express();
 const path = require("path");
 const MongoClient = require("mongodb").MongoClient;
 
-const PORT = 5050;
+const db_username = process.env.MONGO_DB_USERNAME;
+const db_password = process.env.MONGO_DB_PWD;
+const PORT = process.env.APP_PORT || 5050;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const MONGO_URL = "mongodb://admin:qwerty@localhost:27017";
+const MONGO_URL = `mongodb://${db_username}:${db_password}@mongo:27017`;
 const client = new MongoClient(MONGO_URL);
 
 //GET all users
 app.get("/getUsers", async (req, res) => {
-    await client.connect(URL);
-    console.log('Connected successfully to server');
+  await client.connect(MONGO_URL);
+  console.log("Connected successfully to server");
 
-    const db = client.db("apnacollege-db");
-    const data = await db.collection('users').find({}).toArray();
-    
-    client.close();
-    res.send(data);
+  const db = client.db("apnacollege-db");
+  const data = await db.collection("users").find({}).toArray();
+
+  client.close();
+  res.send(data);
 });
 
 //POST new user
 app.post("/addUser", async (req, res) => {
-    const userObj = req.body;
-    console.log(req.body);
-    await client.connect(URL);
-    console.log('Connected successfully to server');
+  const userObj = req.body;
+  console.log(req.body);
 
-    const db = client.db("apnacollege-db");
-    const data = await db.collection('users').insertOne(userObj);
-    console.log(data);
-    console.log("data inserted in DB");
-    client.close();
+  await client.connect(MONGO_URL);
+  console.log("Connected successfully to server");
+
+  const db = client.db("apnacollege-db");
+  const data = await db.collection("users").insertOne(userObj);
+  console.log(data);
+  console.log("data inserted in DB");
+
+  client.close();
+  res.send({ status: "ok" });
 });
 
-
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`server running on port ${PORT}`);
 });
